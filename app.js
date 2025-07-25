1af2eea54ce2243efd4798db22e518d66c6b8c40
sowmyahv1992 <prathik090@gmail.com>
2025-07-25 16:00:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to AWS CI/CD Demo Latest Cheange'));
+app.get('/', (req, res) => res.send('Welcome to AWS CI/CD Demo Latest Cheange 123'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
